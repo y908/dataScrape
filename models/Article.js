23693a421dd60ebecd34c91236f221dcbf6aa425
Yana Yakubchik <Yana Yakubchik>
2016-09-11 21:40:53 -0400

    adding homework files

---
+++ models/Article.js
@@ -0,0 +1,39 @@
+// require mongoose
+var mongoose = require('mongoose');
+// create Schema class
+var Schema = mongoose.Schema;
+
+// Create article schema
+var ArticleSchema = new Schema({
+  // title is required
+  title: {
+    type:String,
+    required:true
+  },
+  // link is required
+  link: {
+    type:String,
+    required:true
+  },
+  // snippet of text, not required
+  snippet: {
+    type:String,
+    required:false
+  },
+  // vidimage, not required
+  vidimage: {
+    type:String,
+    required:false
+  },
+  // this only saves one note's ObjectId. ref refers to the Note model.
+  note: {
+      type: Schema.Types.ObjectId,
+      ref: 'Note'
+  }
+});
+
+// Create the Article model with the ArticleSchema
+var Article = mongoose.model('Article', ArticleSchema);
+
+// export the model
+module.exports = Article;
---
+++ public/app.js
@@ -0,0 +1,13 @@
+// grab the articles as a json
+$.getJSON('/articles', function(data) {
+
+  console.log("app.js file .getJSON runs");
+
+
+  // for each one
+  for (var i = 0; i<data.length; i++){
+    // display the apropos information on the page
+    $('#articles').append('<div class="myarticle"><img src="'+ data[i].vidimage +'"" width="200px"><a href="'+ data[i].link +'"><p data-id="' + data[i]._id + '">'+ data[i].title +'</a><br /><i>' + data[i].snippet +'</i></p><p>Add a comment:</p><form action="submit"><input type="text"><input type="submit"></form></div>');
+  }
+});
+
