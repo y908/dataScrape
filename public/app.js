68d30f2730078e064766196eaa28251060664c92
Yana Yakubchik <Yana Yakubchik>
2016-09-15 21:47:35 -0400

    added http

--- public/app.js
+++ public/app.js
@@ -7,7 +7,7 @@ $.getJSON('/articles', function(data) {
   // for each one
   for (var i = 0; i<data.length; i++){
     // display the apropos information on the page
-    $('#articles').append('<div class="myarticle"><img src="'+ data[i].vidimage +'"" width="200px"><a href="'+ data[i].link +'"><p data-id="' + data[i]._id + '">'+ data[i].title +'</a><br /><i>' + data[i].snippet +'</i></p><p>Add a comment:</p><form action="submit"><input type="text"><input type="submit"></form></div>');
+    $('#articles').append('<div class="myarticle"><img src="http:'+ data[i].vidimage +'"" width="200px"><a href="'+ data[i].link +'"><p data-id="' + data[i]._id + '">'+ data[i].title +'</a><br /><i>' + data[i].snippet +'</i></p><p>Add a comment:</p><form action="submit"><input type="text"><input type="submit"></form></div>');
   }
 });
 
